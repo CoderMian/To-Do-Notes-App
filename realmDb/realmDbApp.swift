9815538127279a67f11722ea7b7957ecf0359e2e
“@Noman786” <muhammadnomantariq1999@gmail.com>
2022-07-05 02:28:12 +0500

    Initial Commit

---
+++ realmDb/realmDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  realmDbApp.swift
+//  realmDb
+//
+//  Created by devsaar on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct realmDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
